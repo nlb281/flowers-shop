e9f5ca646969712c5161204db991effaaed82d05
nlb281 <nlb.marian@gmail.com>
2026-05-20 13:41:51 +0300

    Добавил окно авторизации и настроил его запуск при старте приложения

---
+++ flowersShop/ViewModels/AuthViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using flowersShop.Models;
+using flowersShop.Views;
+using Microsoft.EntityFrameworkCore;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+
+namespace flowersShop.ViewModels;
+
+public class AuthViewModel : ViewModelBase
+{
+    private string login, password;
+
+    public string Login
+    {
+        get => login;
+        set => login = value;
+    }
+
+    public string Password
+    {
+        get => password;
+        set => password = value;
+    }
+
+    public void LogIn()
+    {
+        StaticFields.user = StaticFields.context.Users.Include(x => x.Role)
+            .FirstOrDefault(x => x.Login == Login && x.Password == Password);
+
+        if (StaticFields.user == null)
+        {
+            MessageBoxManager.GetMessageBoxStandard("Information", "Invalid login or password", ButtonEnum.Ok)
+                .ShowWindowAsync();
+        }
+        else
+        {
+            StaticFields.window = StaticFields.oldWindow;
+            (new MainWindow()).Show();
+            StaticFields.window.Close();
+        }
+    }
+}
---
+++ flowersShop/Views/Auth.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using flowersShop.Models;
+using flowersShop.ViewModels;
+
+namespace flowersShop.Views;
+
+public partial class Auth : Window
+{
+    AuthViewModel vm =  new AuthViewModel();
+    
+    public Auth()
+    {
+        InitializeComponent();
+        DataContext = vm;
+        StaticFields.oldWindow = this;
+    }
+}
