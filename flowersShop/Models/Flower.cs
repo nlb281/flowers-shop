d94ba203c34797728752d420adcb22157f049005
nlb281 <nlb.marian@gmail.com>
2026-05-21 10:38:24 +0300

    Добавил свойства IsInCart, CanAddToCart и AddButtonText для управления состоянием кнопки добавления товара в корзину

--- flowersShop/Models/Flower.cs
+++ flowersShop/Models/Flower.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using ReactiveUI;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace flowersShop.Models;
 
-public partial class Flower
+public partial class Flower : ReactiveObject
 {
     public int Id { get; set; }
 
@@ -17,6 +19,7 @@ public partial class Flower
 
     public string ImagePath { get; set; } = null!;
     
+    [NotMapped]
     public Bitmap ImageBitmap =>
         new Bitmap(
             AssetLoader.Open(
@@ -28,5 +31,22 @@ public partial class Flower
 
     public int Quantity { get; set; }
 
+    private bool _isInCart;
+    
+    [NotMapped]
+    public bool IsInCart
+    {
+        get => _isInCart;
+        set => this.RaiseAndSetIfChanged(ref _isInCart, value);
+    }
+    
+    [NotMapped]
+    public string AddButtonText =>
+        IsInCart
+            ? "Товар в корзине"
+            : "Добавить в корзину";
+    
+    [NotMapped]
+    public bool CanAddToCart => !IsInCart;
     public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 }
